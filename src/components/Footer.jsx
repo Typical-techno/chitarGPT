d94bed2df1d0293a2bd0fe04f8e64b765c3349b3
typical-techno <shubhamjangrartk@gmail.com>
2024-06-15 14:51:47 +0530

    added footer <p>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,8 @@
 import React from "react";
-import { socialLink } from "../data/footer";
 
 function Footer() {
   return (
     <div className="px-8 py-12 bg-white space-y-4 items-center">
-      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3 py-16 gap-5 md:gap-0">
-        <div></div>
-        <div className="text-left text-black w-full">
-          <div className="space-y-6">
-            <button className="text-5xl font-bold">
-              <p className="flex items-center">
-                <span>ChitraGPT</span>
-              </p>
-            </button>
-            <div>
-              <p>
-                This is a user friendly Deep Fake Detection tool designed for
-                efficient scanning of suspicious video/image files to find out
-                any manipulation.
-              </p>
-            </div>
-            <div className="flex items-center text-black">
-              {socialLink.map((items, index) => (
-                <div key={index} className="flex flex-row">
-                  <a href={`${items.link}`}>
-                    <button className="px-2">
-                      <p className="text-3xl text-black">
-                        <i className={`fi ${items.icon}`}></i>
-                      </p>
-                    </button>
-                  </a>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="text-sm md:text-base text-black">
         <p>
           &copy; Copyright <span className="font-bold">GPCSSI</span>. All Rights
